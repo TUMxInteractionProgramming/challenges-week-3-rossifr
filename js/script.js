38ac269e170a63dff8ed90f27667671e95adaccc
rossifr <franco.f.rossi@gmail.com>
2018-09-22 00:41:03 +0200

    test

--- js/script.js
+++ js/script.js
@@ -2,4 +2,5 @@ console.log('app is alive');
 
 function switchChannel(channelName){
   console.log('Tuning into channel ' + channelName);
+  document.getElementById('chat').querySelector("h1").innerHTML=channelName;
 }
